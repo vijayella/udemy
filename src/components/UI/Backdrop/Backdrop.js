b8dbbf37117ec0eb1cd7fcae24d29607bc2d87a6
Vijaykumar Yella <yella.vijaykumar@gmail.com>
2018-04-25 14:31:46 +0530

    Removed code

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import classes from './Backdrop.css';
-
-const backdrop = (props) => (
-    props.show ? <div className={classes.BackDrop} onClick={props.clicked}></div> : null
-);
-
-export default backdrop;
